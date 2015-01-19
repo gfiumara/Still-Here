b8c735fdec8cf80eb6e1c118104ee92de20e575f
Greg Fiumara <greg@gregfiumara.com>
2015-01-19 10:25:38 -0500

    Inital add.

---
+++ Still Here/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Still Here
+//
+//  Created by Greg Fiumara on 1/19/15.
+//  Copyright (c) 2015 Greg Fiumara. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Still Here/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Still Here
+//
+//  Created by Greg Fiumara on 1/19/15.
+//  Copyright (c) 2015 Greg Fiumara. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
