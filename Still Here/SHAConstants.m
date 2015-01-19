96f3e8746cf1f53b4bbdb7625fd1936c8aab48ae
Greg Fiumara <greg@gregfiumara.com>
2015-01-19 11:39:01 -0500

    Add placeholder for Nest Client ID.

---
+++ Still Here/SHAConstants.m
@@ -0,0 +1,12 @@
+//
+//  SHAConstants.m
+//  Still Here
+//
+//  Created by Greg Fiumara on 1/19/15.
+//  Copyright (c) 2015 Greg Fiumara. All rights reserved.
+//
+
+#import "SHAConstants.h"
+
+NSString * const kSHANestAPIClientID = @"<# Enter your Client ID here #>";
+NSString * const kSHANestAccessTokenKey = @"access_token";
